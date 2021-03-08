4e43346404e4e677cccdb9344586c1361e2a989a
Robert Barentt <robertbarnett06@gmail.com>
2021-03-08 12:37:18 -0800

    Did more to make cmake better

---
+++ CMakeFiles/projectRunable.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/home/robertbarnett/Documents/programming/ccpp/bin/projectRunable"
+  "/home/robertbarnett/Documents/programming/ccpp/bin/projectRunable.pdb"
+  "CMakeFiles/projectRunable.dir/src/add.cpp.o"
+  "CMakeFiles/projectRunable.dir/src/ball.cpp.o"
+  "CMakeFiles/projectRunable.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projectRunable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
