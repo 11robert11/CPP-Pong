d1a7377e8af03a721f6c6c06a00d523dee76bb9b
Robert Barentt <robertbarnett06@gmail.com>
2021-03-11 08:49:46 -0800

    Did stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,29 @@
-#include "add.h"
 #include <iostream>
-
+#include <ostream>
+#include <time.h>
+using namespace std;
+void delay(int number_of_seconds) 
+{ 
+    // Converting time into milli_seconds 
+    int milli_seconds = 1000 * number_of_seconds; 
+  
+    // Storing start time 
+    clock_t start_time = clock(); 
+  
+    // looping till required time is not achieved 
+    while (clock() < start_time + milli_seconds); 
+} 
 int main()
-{
+{	
+    for(int i = 0; i<100000; i = i+1)
+    {
+    cout << "N";
+    delay(1);
+    cout << "\b";
+    delay(1);
+    cout << "";
 
-    std::cout << "Hello World" << "\n";
-    std::cout << add(10, 20) << "\n";
-
-    return 0;
+    }
+    cout << "Done" << endl;
+	return(0);
 }
